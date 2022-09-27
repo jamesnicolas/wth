fc60264faed22309ea6f918bb26f49e20d54d3ff
James Nicolas <jnicolas@league.com>
2022-09-26 22:40:44 -0400

    changed up the types a bit

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
 use dialoguer::{theme::ColorfulTheme, FuzzySelect};
 
-enum Bookmark<'a> {
-    Folder(String, &'a Vec<String>),
-    Link(String),
-    Search(String),
+enum Content<'a> {
+    Folder(&'a [Bookmark<'a>]),
+    Link(&'a str),
+    Search(&'a str),
+}
+
+struct Bookmark<'a> {
+    title: &'a str,
+    content: Content<'a>
+}
+
+impl<'a> Bookmark<'a> {
+    fn items(&'a self) -> Vec<String> {
+        match self.content {
+            Content::Folder(folder) => {
+                let content = folder.iter().map(|item| item.title.to_string()).collect::<Vec<String>>();
+                content
+            },
+            Content::Link(link) => vec!(link.to_string()),
+            Content::Search(search) => vec!(search.to_string()),
+        }
+    }
+
 }
 
 fn main() {
-    let folder = vec!(String::from("google.com"), String::from("github.com"));
-    let default = Bookmark::Folder("default".to_string(), &folder);
+    let root = Bookmark{
+        title: "root",
+        content: Content::Folder(&[Bookmark{title: "google.com", content: Content::Link("google.com")}]),
+    };
 
     let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Select bookmark")
         .default(0)
-        .items(if let Bookmark::Folder(_, list) = default{list} else {&[]})
+        .items(&root.items())
         .interact()
         .unwrap();
 
-    println!("You picked {}", folder[selection]);
+    println!("You picked {}", selection);
 
 }
